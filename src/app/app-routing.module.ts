794a26bb88c3ae8a07a61b84fb4c2acb3ff842af
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-04-01 09:31:20 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { ColorComponent } from './components/color/color.component';
 import { ResultComponent } from './components/result/result.component';
 import { TextComponent } from './components/text/text.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/text', pathMatch: 'full' },
+  { path: '**', redirectTo: '/text' }, // ** = quando l'url è sbagliato. Di solito si rimanda a una page not found semplice, in html e css.
   { path: 'text', component: TextComponent },
   { path: 'color', loadChildren: () => import('./components/color/module/color.module').then(m => m.ColorModule) },
   { path: 'result', loadChildren: () => import('./components/result/module/result.module').then(m => m.ResultModule)},
   // { path: 'color', component: ColorComponent },
   // { path: 'result', component: ResultComponent },
-
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {
+      preloadingStrategy: PreloadAllModules
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
